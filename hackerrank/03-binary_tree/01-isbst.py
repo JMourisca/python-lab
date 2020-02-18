5fdc4fb1be1d46d1e9697d1bfd93c84433f0da22
Juliana Mourisca <juliana.mourisca@starticket.ch>
2020-02-18 16:20:23 +0100

    Fixed garbage bst function

--- hackerrank/03-binary_tree/01-isbst.py
+++ hackerrank/03-binary_tree/01-isbst.py
@@ -5,22 +5,26 @@ class Node:
         self.right = None
 
 def check_binary_search_tree_(root): 
-    if root is None:
+    def check_binary_search_tree(node, lower, upper):
+        if not node:
+            return True
+
+        data = node.data
+        isBst = True
+
+        if data <= lower or data >= upper:
+            return False
+
+        if not check_binary_search_tree(node.right, data, upper):
+            return False
+        
+        if not check_binary_search_tree(node.left, lower, data):
+            return False
         return True
 
-    left = root.left    
-    right = root.right
-    data = root.data 
-    isBst = True
-
-    if not right and not left:
-        return True
-
-    if (right and right.data <= data) or (left and left.data >= data):
-        return False
-
-    return isBst and check_binary_search_tree_(left) and check_binary_search_tree_(right)
+    return check_binary_search_tree(root, float("-inf"), float("inf"))
     
+
 root = Node(100)
 
 root.left = Node(5)
@@ -31,7 +35,7 @@ root.right = Node(13)
 root.right.left = Node(11)
 root.right.right = Node(18)
 
-print(check_binary_search_tree_(root))
+print(check_binary_search_tree_(root) == False) # false
 
 root = Node(10)
 
@@ -42,11 +46,10 @@ root.left.right = Node(6)
 root.right = Node(13)
 root.right.left = Node(11)
 root.right.right = Node(18)
-print(check_binary_search_tree_(root))
+print(check_binary_search_tree_(root) == False) # false
 
 root = Node(1)
-print(check_binary_search_tree_(root))
-
+print(check_binary_search_tree_(root) == True) # true
 
 root = Node(10)
 
@@ -59,7 +62,7 @@ root.right.left = Node(11)
 root.right.right = Node(18)
 root.right.right.right = Node(19)
 
-print(check_binary_search_tree_(root))
+print(check_binary_search_tree_(root) == True) # true
 
 root = Node(10)
 root.left = Node(5)
@@ -68,4 +71,17 @@ root.left.left.left = Node(3)
 root.left.left.left.left = Node(2)
 root.left.left.left.left.left = Node(1)
 root.right = Node(11)
-print(check_binary_search_tree_(root))
+print(check_binary_search_tree_(root) == True) # true
+
+root = Node(10)
+
+root.left = Node(5)
+root.left.left = Node(3)
+root.left.right = Node(6)
+
+root.right = Node(13)
+root.right.left = Node(9)
+root.right.right = Node(18)
+root.right.right.right = Node(19)
+
+print(check_binary_search_tree_(root) == False) # false
